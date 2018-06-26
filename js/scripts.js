e479eff0261b5ee831dc7331492128b9db5866fc
Virlie Paglinawan <virliejoyp@yahoo.com>
2018-06-26 06:46:04 -0700

    add and finish business logic to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,18 @@ function inputToArray(inputNumber) { //make input into array
   }
   return convertToPingPong();
 }
+function convertToPingPong() { //convert array items to ping/pong/pingpong
+  var index = 0;
+  countingArray.forEach(function(number) {
+    if (number % 15 === 0) {
+      countingArray[index] = "class='pingpong'>\u221EPINGPONG\u221E";
+    } else if (number % 3 === 0) {
+      countingArray[index] = "class='ping'>\u2022PING\u2022";
+    } else if (number % 5 === 0) {
+      countingArray[index] = "class='pong'>\u00B0PONG\u00B0";
+    } else {
+      countingArray[index] = "class='number'>"+number
+    }
+    index++;
+  });
+}
